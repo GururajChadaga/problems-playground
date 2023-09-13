3920f791db4c7914bcf15165bc540e48c2a56959
Gururaj Chadaga <gururajchadaga@Gururajs-MacBook-Air.local>
2023-09-13 13:58:07 +0530

    maxSubArrayProduct

---
+++ src/problems/maxSubArrayProduct/index.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// Without Kadanes
+const maxProduct = function (nums) {
+  let currentMaxProduct = -Infinity,
+    productFromLeft = 1,
+    productFromRight = 1;
+  for (let index = 0; index < nums.length; index++) {
+    const valueFromLeft = nums[index];
+    const valueFromRight = nums[nums.length - index - 1];
+    if (productFromLeft === 0) productFromLeft = 1;
+    if (productFromRight === 0) productFromRight = 1;
+    productFromLeft *= valueFromLeft;
+    productFromRight *= valueFromRight;
+    currentMaxProduct = Math.max(
+      currentMaxProduct,
+      productFromLeft,
+      productFromRight
+    );
+  }
+  return currentMaxProduct;
+};
