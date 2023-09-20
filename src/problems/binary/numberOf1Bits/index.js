168c7f9b4bb53efa9c974e04117c64b77caaef37
Gururaj Chadaga <gururajchadaga@Gururajs-MacBook-Air.local>
2023-09-20 23:33:06 +0530

    binary: numberOf1Bits optimal

--- src/problems/binary/numberOf1Bits/index.js
+++ src/problems/binary/numberOf1Bits/index.js
@@ -19,3 +19,16 @@ const hammingWeight = function (n) {
 const hammingWeightStringManipulation = function (n) {
   return n.toString(2).split('0').join('').length;
 };
+
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+const hammingWeightOptimal = function (n) {
+  let hammingWeight = 0;
+  while (n) {
+    hammingWeight++;
+    n &= n - 1;
+  }
+  return hammingWeight;
+};
