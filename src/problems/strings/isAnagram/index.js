70094bac8e09abb64c2e7ed82a02a8cb6fd0dc00
Gururaj Chadaga <guruuraj.chadaga@gmail.com>
2023-10-11 08:49:09 +0530

    strings: isAnagram

---
+++ src/problems/strings/isAnagram/index.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+
+/*
+  Time: O(n)
+    n = s.length = t.length
+  Space: O(n)
+ */
+const isAnagram = function (s, t) {
+  let sFreqMap = {},
+    tFreqMap = {};
+  if (s.length !== t.length) return false;
+  for (let i = 0; i < s.length; i++) {
+    sFreqMap[s[i]] = sFreqMap[s[i]] ? sFreqMap[s[i]] + 1 : 1;
+    tFreqMap[t[i]] = tFreqMap[t[i]] ? tFreqMap[t[i]] + 1 : 1;
+  }
+  for (let key in sFreqMap) {
+    if (sFreqMap[key] !== tFreqMap[key]) return false;
+  }
+  return true;
+};
+
+/*
+  Time: O(nlogn)
+    n = s.length = t.length
+    split and join takes n each
+    logn to sort
+  Space: O(1) or O(n) based on which sort is implemented
+ */
+const isAnagramalt = function (s, t) {
+  return s.split('').sort().join('') === t.split('').sort().join('');
+};
