b0b878540b152c4f9fd9cbd4544dae7865fefea7
Gururaj Chadaga <guruuraj.chadaga@gmail.com>
2023-10-11 09:44:15 +0530

    strings: groupAnagrams

---
+++ src/problems/strings/groupAnagrams/index.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+
+/*
+  Time: O(n * nlogn)
+    n for loop
+    nlogn for sorting n strings
+  Space: O(k)
+    k is the number of anagrams for which k-v pairs are stored in the map/object
+ */
+const groupAnagrams = function (strs) {
+  const groupedAnagrams = {};
+  for (let str of strs) {
+    const sortedStr = str.split('').sort().join('');
+    if (!groupedAnagrams[sortedStr]) groupedAnagrams[sortedStr] = [];
+    groupedAnagrams[sortedStr].push(str);
+  }
+  return Object.values(groupedAnagrams);
+};
+
+const groupAnagramsUsingMap = function (strs) {
+  const groupedAnagrams = new Map();
+  for (let str of strs) {
+    const count = new Array(26).fill(0);
+    const sortedStr = str.split('').sort().join('');
+    const groupedArr = groupedAnagrams.get(sortedStr);
+    const updatedGroupedArr = groupedArr ? [...groupedArr, str] : [str];
+    groupedAnagrams.set(sortedStr, updatedGroupedArr);
+  }
+  return [...groupedAnagrams.values()];
+};
