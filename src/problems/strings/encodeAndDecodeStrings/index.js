e85ef280d068bffbc65b9ad4eff6deae330b0d0c
Gururaj Chadaga <guruuraj.chadaga@gmail.com>
2023-10-12 09:27:31 +0530

    strings: encodeAndDecodeStrings

---
+++ src/problems/strings/encodeAndDecodeStrings/index.js
@@ -0,0 +1,23 @@
+const strs = ['apple', '4oranges', '#85fives'];
+
+const encode = (strs) => {
+  return strs.reduce((acc, str) => `${acc}${str.length}${str}`, '');
+};
+
+const decode = (encoded) => {
+  const decoded = [];
+  let i = 0;
+  while (i < encoded.length) {
+    const length = +encoded[i];
+    decoded.push(encoded.slice(i + 1, i + 1 + length));
+    i += length + 1;
+  }
+  return decoded;
+};
+
+const encoded = encode(strs);
+const decoded = decode(encoded);
+
+console.log('strs:', strs);
+console.log('encoded:', encoded);
+console.log('decoded:', decoded);
