698805de14375e93508bcd11f934088e69e5ed17
Gururaj Chadaga <guruuraj.chadaga@gmail.com>
2023-10-20 16:18:31 +0530

    trees: BT maximumPathSum

---
+++ src/problems/trees/maximumPathSum/index.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const maxPathSum = function (root) {
+  let maxPathSum = -Infinity;
+
+  const dfs = (node) => {
+    if (!node) return 0;
+
+    // if sum is negative, take 0
+    const leftMaxPathSum = Math.max(0, dfs(node.left)),
+      rightMaxPathSum = Math.max(0, dfs(node.right));
+    maxPathSum = Math.max(
+      maxPathSum,
+      node.val + leftMaxPathSum + rightMaxPathSum
+    );
+    return node.val + Math.max(leftMaxPathSum, rightMaxPathSum);
+  };
+
+  dfs(root);
+  return maxPathSum;
+};
